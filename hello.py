e59ba7a2972a6ce57f6759e7282d2c4d491f2fcf
huzaifak4364 <huzaifak4364@gmail.com>
2022-12-07 04:27:18 -0500

    discord bot

---
+++ hello.py
@@ -0,0 +1,44 @@
+
+import discord
+import openai
+
+# Set up the OpenAI API key
+openai.api_key = "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"
+# Create a new Discord client
+#client = discord.Client(intents=discord.Intents.default())
+
+#TypeError: __init__() missing 1 required keyword-only argument: 'intents'
+
+client = discord.Client(intents=discord.Intents.all())
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+@client.event
+
+
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt= message.content,
+        temperature=0.75,
+        max_tokens=4000,
+        
+        
+    )
+
+    
+    #print(response)
+    #print(message.content)
+
+    await message.channel.send(response['choices'][0]['text'])
+    
+
+
+
+# Run the Discord client
+client.run("MTA0OTkzMjk3MTg3MzkyNzE4OA.G4aRjq.q-HXATnEHYBy00Lw8efSdYGazw-dXMBWaVdvio")
